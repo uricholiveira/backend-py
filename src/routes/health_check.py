9a8fff44e070839290dce2101dcce2e77eaf5b51
Urich Oliveira <oliveira.urich@gmail.com>
2023-04-04 16:13:00 -0300

    Adding CI Actions | health_check endpoint fixed

--- src/routes/health_check.py
+++ src/routes/health_check.py
@@ -1,21 +1,18 @@
-from dependency_injector.wiring import inject
-from fastapi import APIRouter
+from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends
+
+from src.common.containers import Container
+from src.services.health_check import HealthCheckService
 
 router = APIRouter(prefix="/health_check", tags=["Health Check"])
 
 
 @router.get("/")
 @inject
-async def health_check():
-    return "OK"
-
-
-# Example of inject on endpoints
-
-# @router.get("/")
-# @inject
-# async def health_check(
-#     health_check_service: HealthCheckService = Depends(Provide[Container.health_check_service]),
-# ):
-#     result = await health_check_service.get_status()
-#     return result
+async def health_check(
+    health_check_service: HealthCheckService = Depends(
+        Provide[Container.health_check_service]
+    ),
+):
+    result = await health_check_service.get_status()
+    return result
